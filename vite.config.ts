86c5fda3cf47a7d87fe964398f1327d3243704dd
Andriy Shymkiv <winamondezik@gmail.com>
2023-07-06 14:41:05 +0300

    chore: change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+  define: { 'process.env': {} },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: { global: 'globalThis' },
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true, process: true })],
+    },
+  },
 });
