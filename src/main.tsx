6fd8dc899c05c9755d75dade8ef1fca1f8cf9cfa
Andriy Shymkiv <winamondezik@gmail.com>
2023-07-06 14:46:15 +0300

    feat: separate react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,24 @@
 import { ThemeProvider } from '@mui/material';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from './App';
+import { ReactQueryProvider } from './providers/ReactQueryProvider';
 import { WalletProvider } from './providers/WalletProvider';
 import { store } from './store';
 import './styles/index.css';
 import { theme } from './theme';
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
+      <ReactQueryProvider>
         <WalletProvider>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
-          <ReactQueryDevtools initialIsOpen={false} />
         </WalletProvider>
-      </QueryClientProvider>
+      </ReactQueryProvider>
     </Provider>
   </React.StrictMode>,
 );
